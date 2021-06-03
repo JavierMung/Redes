9fa7dc665a0588e9b3325856822471e45adeda8a
javier <javiermunguia35@gmail.com>
2021-06-02 19:34:09 -0500

    Pruebas

--- arp.c
+++ arp.c
@@ -1,24 +1,32 @@
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <sys/time.h>
+void enviaTrama(int, int, unsigned char *); 
 void estructuraARPsol(unsigned char*);
-
-unsigned char tramaARPsol[60] = {0xff,0xff,0xff,0xff,0xff,0xff,0x0A,0x00,0x27,0x00,0x00,0x02,
-	0x08,0x06,0x00,0x01,0x08,0x00,0x06,0x04,0x00,0x01,0x0A,0x00,0x27,0x00,0x00,0x02,0xc0,0xa8,0x00,0x01,
-	0x00,0x00,0x00,0x00,0x00,0x00,0xc0,0xa8,0x00,0x02,'F','J','M','G'};
+void imprimirTrama(char*,int);
+unsigned char tramaARPsol[60] = {0xff,0xff,0xff,0xff,0xff,0xff,0x0A,0x00,0x27,0x00,0x00,0x02,0x08,0x06,0x00,0x01,0x08,0x00,0x06,0x04,0x00,0x01,0x0A,0x00,0x27,0x00,0x00,0x02,0xc0,0xa8,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0xc0,0xa8,0x00,0x02,'F','J','M','G'};
 
 
 int main(int argc, int ** argv){
 
 	int packet_socket, indice;
-	packet_socket=socket(AF_SOCKET,SOCK_RAW,htons(ETH_P_ALL));
+	packet_socket=socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
 	if(packet_socket==-1){
-		perror("nError al abrir el socket\n");
+		perror("\nError al abrir el socket\n");
 		exit(0);
 	}else{
-		printf("\nExitoal abrir el socket\n");
-		indice=obtenerDatos(packet_socket);
-		obtenerIPdestino();
-		estructuraARPsol(tramaARPsol);
+		printf("\nExito al abrir el socket\n");
+	//	indice=obtenerDatos(packet_socket);
+	//	obtenerIPdestino();
+	//	estructuraARPsol(tramaARPsol);
 		printf("\nLa trama que se enviara es: \n");
 		imprimirTrama(tramaARPsol,60);
 		enviaTrama(packet_socket,indice,tramaARPsol);
@@ -28,7 +36,7 @@ int main(int argc, int ** argv){
 	close(packet_socket);
 	return 1;
 }
-
+/*
 void estructuraARPsol(unsigned char * trama){
 	memcpy(trama+6,MACorigen,6);
 	memcpy(trama+22,MACorigen,4);
@@ -36,7 +44,7 @@ void estructuraARPsol(unsigned char * trama){
 	memset(trama+32,0x00,6);
 	memcpy(trama+38,IPdestino,4);
 }
-
+*/
 void enviaTrama(int ds, int index, unsigned char * paq){
 	int tam;
 	struct sockaddr_ll capaEnlace;
@@ -44,11 +52,20 @@ void enviaTrama(int ds, int index, unsigned char * paq){
 	capaEnlace.sll_family = AF_PACKET;
 	capaEnlace.sll_protocol = htons(ETH_P_ALL);
 	capaEnlace.sll_ifindex = index;
-	tam = sento(ds,paq,60,0,(struct sockaddr*)&capaEnlace,sizeof(capaEnlace));
+	tam = sendto(ds,paq,60,0,(struct sockaddr*)&capaEnlace,sizeof(capaEnlace));
 	if(tam==-1){
 		perror("\nError al enviar la trama\n");
 	}else{
 		printf("nExito al enviar la trama\n");
 		
 	}
+}
+
+void imprimirTrama(char * trama, int cantidad){
+	int i = 0;
+	
+	for(i = 0; i<cantidad; i++){
+		printf("%d",trama[i]);
+	}
+
 }
